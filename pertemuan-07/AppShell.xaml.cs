9b9cb0569cbdd5116d64e6348292fa458264c39e
H-mD <syahrizalpmg@gmail.com>
2023-10-15 11:49:35 +0700

    feat: add maui.NET android app

---
+++ pertemuan-07/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace pertemuan_07
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ pertemuan-07/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace pertemuan_07
+{
+    public static class MauiProgram
+    {
+        public static MauiProgram CreateMauiApp()
+        {
+            var builder = MauiProgram.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
